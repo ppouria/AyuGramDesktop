aafab2df8f33b30335c3cf1bf3d59a374ebae9f7
Pouria Ebrahimzade <pouriaebrahimzade@yahoo.com>
2026-08-03 21:04:11 +0330

    Stabilize and speed up Windows debug startup

--- Telegram/SourceFiles/core/launcher.cpp
+++ Telegram/SourceFiles/core/launcher.cpp
@@ -120,10 +120,6 @@ void ComputeDebugMode() {
 	auto file = QFile(debugModeSettingPath);
 	if (file.exists() && file.open(QIODevice::ReadOnly)) {
 		Logs::SetDebugEnabled(file.read(1) != "0");
-#if defined _DEBUG && !defined Q_OS_MAC
-	} else {
-		Logs::SetDebugEnabled(true);
-#endif
 	}
 	if (cDebugMode()) {
 		Logs::SetDebugEnabled(true);
