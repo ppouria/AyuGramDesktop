c38ce82fe9bbe07f562c0efdc354e5d6df826457
Pouria Ebrahimzade <pouriaebrahimzade@yahoo.com>
2026-08-04 12:42:51 +0330

    Restore MTP update checker fallback

--- Telegram/SourceFiles/core/update_checker.cpp
+++ Telegram/SourceFiles/core/update_checker.cpp
@@ -1488,6 +1488,9 @@ void Updater::start(bool forceWait) {
 		startImplementation(
 			&_httpImplementation,
 			std::make_unique<HttpChecker>(_testing));
+		startImplementation(
+			&_mtpImplementation,
+			std::make_unique<MtpChecker>(_session, _testing));
 		_checking.fire({});
 	} else {
 		_timer.callOnce((updateInSecs + 5) * crl::time(1000));
