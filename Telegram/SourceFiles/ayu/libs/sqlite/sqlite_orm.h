694858bb82eb99e995bed46f0ebef9e55c84ac83
Pouria Ebrahimzade <pouriaebrahimzade@yahoo.com>
2026-08-04 11:40:39 +0330

    Fix GCC 15 sqlite ORM warnings

--- Telegram/SourceFiles/ayu/libs/sqlite/sqlite_orm.h
+++ Telegram/SourceFiles/ayu/libs/sqlite/sqlite_orm.h
@@ -11260,7 +11260,7 @@ namespace sqlite_orm {
             [[nodiscard]] consteval auto quote(F callable) const {
                 using Sig = function_signature_type_t<decltype(&F::operator())>;
                 // detect whether overloaded call operator can be picked using `Sig`
-                using call_operator_type = decltype(static_cast<Sig F::*>(&F::operator()));
+                using call_operator_type [[maybe_unused]] = decltype(static_cast<Sig F::*>(&F::operator()));
                 return quoted_scalar_function<F, Sig, N>{this->cstr, std::move(callable)};
             }
 
@@ -11271,7 +11271,7 @@ namespace sqlite_orm {
                 requires ((stateless<F> || std::copy_constructible<F>))
             [[nodiscard]] consteval auto quote(F callable) const {
                 // detect whether overloaded call operator can be picked using `Sig`
-                using call_operator_type = decltype(static_cast<Sig F::*>(&F::operator()));
+                using call_operator_type [[maybe_unused]] = decltype(static_cast<Sig F::*>(&F::operator()));
                 return quoted_scalar_function<F, Sig, N>{this->cstr, std::move(callable)};
             }
 
@@ -11292,7 +11292,7 @@ namespace sqlite_orm {
                 requires ((stateless<F> || std::copy_constructible<F>))
             [[nodiscard]] consteval auto quote(Args&&... constructorArgs) const {
                 // detect whether overloaded call operator can be picked using `Sig`
-                using call_operator_type = decltype(static_cast<Sig F::*>(&F::operator()));
+                using call_operator_type [[maybe_unused]] = decltype(static_cast<Sig F::*>(&F::operator()));
                 return quoted_scalar_function<F, Sig, N>{this->cstr, std::forward<Args>(constructorArgs)...};
             }
         };
@@ -21928,7 +21928,7 @@ namespace sqlite_orm {
         // F O::*
         template<typename F, typename ColRef, satisfies<std::is_member_pointer, ColRef> = true>
         static auto make_cte_column(std::string name, const ColRef& finalColRef) {
-            using object_type = table_type_of_t<ColRef>;
+            using object_type [[maybe_unused]] = table_type_of_t<ColRef>;
             using column_type = column_t<ColRef, empty_setter>;
 
             return column_type{std::move(name), finalColRef, empty_setter{}};
